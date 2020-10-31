33eff973d091b6a7021436f7876bbeabdcc0e7be
sonntpd02723 <sonntpd02723@fpt.edu.vn>
2020-11-01 01:58:47 +0700

    up

--- app.js
+++ app.js
@@ -45,5 +45,43 @@ app.use('/', indexRouter);
 
 // error handler
 
-app.listen(process.env.PORT || 3000)
+http.createServer(function (req, res) { 
+  res.writeHead(200, { 'Content-Type': 'text/html' }); 
+
+  // req.url stores the path in the url 
+  var url = req.url; 
+  if (url === "/") { 
+// fs.readFile looks for the html file 
+// the first parameter is the path to the html page 
+// the second is the call back function 
+// if no file is found the function gives an err 
+// if the file is successfully found, the content of the file are contained in pgres 
+      fs.readFile("head.html", function (err, pgres) { 
+          if (err) 
+              res.write("HEAD.HTML NOT FOUND"); 
+          else { 
+              // The following 3 lines 
+              // are reponsible for sending the html file 
+              // and ends the response process 
+              res.writeHead(200, { 'Content-Type': 'text/html' }); 
+              res.write(pgres); 
+              res.end(); 
+          } 
+      }); 
+  } 
+  else if (url === "/tailPage") { 
+      fs.readFile("tail.html", function (err, pgres) { 
+          if (err) 
+              res.write("TAIL.HTML NOT FOUND"); 
+          else { 
+              res.writeHead(200, { 'Content-Type': 'text/html' }); 
+              res.write(pgres); 
+              res.end(); 
+          } 
+      }); 
+  } 
+
+}).listen(process.env.PORT || 3000, function () { 
+  console.log("SERVER STARTED PORT: 3000"); 
+});
 
